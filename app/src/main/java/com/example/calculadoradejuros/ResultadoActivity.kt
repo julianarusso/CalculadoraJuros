6973c5ba8eee3d9c3b2a096dffa0c5ee6cf1ed8e
Julianarusso <juliana.russo3105@gmail.com>
2024-03-07 16:47:37 -0300

    first version

---
+++ app/src/main/java/com/example/calculadoradejuros/ResultadoActivity.kt
@@ -0,0 +1,22 @@
+package com.example.calculadoradejuros
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+        setSupportActionBar(findViewById(R.id.toolbar2))
+        val montante = findViewById<TextView>(R.id.edtMont)
+        val valor = findViewById<TextView>(R.id.edtTotalJuros)
+        val parcela = findViewById<TextView>(R.id.edtValorParcela)
+        val valormont = intent.getDoubleExtra("calcmont", 0.0)
+        val valortotal = intent?.getDoubleExtra("calcvalor", 0.0)
+        val valorparcela = intent?.getDoubleExtra("calcparcela", 0.0)
+        montante.text = valormont.toString()
+        valor.text = valortotal.toString()
+        parcela.text = valorparcela.toString()
+    }
+}
